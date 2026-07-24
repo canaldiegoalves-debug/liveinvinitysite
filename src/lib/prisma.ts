70e639968367ece4e7b09dd2962cc4d316d635e8
Diego Alves <afiliadodiegoalves@gmail.com>
2026-07-23 21:38:56 -0300

    fix: atualiza URL de conexao do Prisma Client para DATABASE_URL

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  const url = process.env.POOLER_URL || "postgresql://postgres.aoifhzglajhnifjqcfqt:Valora2024SaaS!@aws-1-sa-east-1.pooler.supabase.com:6543/postgres?pgbouncer=true";
+  const url = process.env.DATABASE_URL || process.env.POSTGRES_URL || process.env.POOLER_URL || "postgresql://postgres:Valora2024SaaS!@db.aoifhzglajhnifjqcfqt.supabase.co:5432/postgres";
   console.log("PRISMA INITIALIZING WITH URL:", url ? url.split('@')[1] : "UNDEFINED");
   return new PrismaClient({
     datasources: {
